a97add9f31e14435cad24673699cde621994b5e8
CitralFlo <wojtas.michal90@gmail.com>
2024-05-20 12:00:07 +0200

    Add more tests in IndexControllerTest

--- src/test/java/dev/citralflo/animelist/contollers/IndexControllerTest.java
+++ src/test/java/dev/citralflo/animelist/contollers/IndexControllerTest.java
@@ -1,14 +1,20 @@
 package dev.citralflo.animelist.contollers;
 
+import dev.citralflo.animelist.model.Series;
 import dev.citralflo.animelist.repositories.SeriesRepository;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class IndexControllerTest {
 
@@ -29,12 +35,23 @@ class IndexControllerTest {
     @Test
     void getIndex() {
 
+        Set<Series> series = new HashSet<>();
+        series.add(new Series());
 
+        Series series1 = new Series();
+        series1.setId(1L);
+        series.add(series1);
+
+        when(seriesRepository.findAll()).thenReturn(series);
+
+        ArgumentCaptor<Set<Series>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         String index = controller.getIndex(model);
 
         assertEquals("index", index);
         verify(seriesRepository, times(1)).findAll();
-        verify(model, times(1)).addAttribute("animes", seriesRepository.findAll());
+        verify(model, times(1)).addAttribute(eq("animes"), argumentCaptor.capture());
+        Set<Series> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
