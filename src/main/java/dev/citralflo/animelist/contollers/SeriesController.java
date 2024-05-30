fef51bc62c82bbcb420e78ed9167a5cf795d5c4e
CitralFlo <wojtas.michal90@gmail.com>
2024-05-30 12:24:40 +0200

    Add form, and controller

--- src/main/java/dev/citralflo/animelist/contollers/SeriesController.java
+++ src/main/java/dev/citralflo/animelist/contollers/SeriesController.java
@@ -1,11 +1,15 @@
 package dev.citralflo.animelist.contollers;
 
+import dev.citralflo.animelist.commands.SeriesCommand;
 import dev.citralflo.animelist.services.SeriesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Slf4j
@@ -24,5 +28,18 @@ public class SeriesController {
         return "series/view";
     }
 
+    @RequestMapping("/series/new")
+    public String newSeries(Model model) {
+        model.addAttribute("series", new SeriesCommand());
 
+        return "series/form";
+    }
+
+    @PostMapping
+    @RequestMapping("series/save")
+    public String saveOrUpdate(@ModelAttribute SeriesCommand command) {
+        SeriesCommand savedCommand = seriesService.saveSeriesCommand(command);
+
+        return "redirect:/series/view/" + savedCommand.getId();
+    }
 }
